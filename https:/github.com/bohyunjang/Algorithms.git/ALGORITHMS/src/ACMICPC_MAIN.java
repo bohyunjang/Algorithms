102b9f2a5979a1062669a7d786b85e889f8ea1f2
jangbohyun <jangbohyun@192.168.0.29>
2017-08-20 23:46:57 +0900

    START ALGORITHMS 170820

---
+++ https:/github.com/bohyunjang/Algorithms.git/ALGORITHMS/src/ACMICPC_MAIN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * 2017 7월 14일 알고리즘 스터디 과제
+ */
+
+public class ACMICPC_MAIN {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int TestCase = 0;
+		
+				
+        TestCase = sc.nextInt(); // 테스트 케이스 입
+		
+        
+        
+	}
+}
+
+
